e8cd7569dfdfb60825bd5049ba4d116acd6654d6
Arthur Gabriel <arthurelgg@gmail.com>
2022-09-07 14:49:02 -0300

    Database

---
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -0,0 +1,107 @@
+package com.devsuperior.dsmeta.entities;
+
+import org.springframework.format.annotation.NumberFormat;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale implements Serializable {
+
+    public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "seller_name", length = 50)
+    private String sellerName;
+    private Integer visited;
+    private Integer deals;
+    @NumberFormat(pattern = "$###,###,###.00")
+    private Double amount;
+    private LocalDate date;
+
+    public Sale() {
+    }
+
+    public Sale(Long id, String sellerName, Integer visited, Integer deals, Double amount, LocalDate date) {
+        this.id = id;
+        this.sellerName = sellerName;
+        this.visited = visited;
+        this.deals = deals;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public Integer getVisited() {
+        return visited;
+    }
+
+    public void setVisited(Integer visited) {
+        this.visited = visited;
+    }
+
+    public Integer getDeals() {
+        return deals;
+    }
+
+    public void setDeals(Integer deals) {
+        this.deals = deals;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Sale)) return false;
+        Sale sale = (Sale) o;
+        return Objects.equals(getId(), sale.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Sale{" +
+                "id=" + id +
+                ", sellerName='" + sellerName + '\'' +
+                ", visited=" + visited +
+                ", deals=" + deals +
+                ", amount=" + amount +
+                ", date=" + date +
+                '}';
+    }
+}
