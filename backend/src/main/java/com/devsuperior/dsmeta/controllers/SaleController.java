62bba3366611f6bdbbacb4551385fed6043d855e
Arthur Gabriel <arthurelgg@gmail.com>
2022-09-07 16:33:38 -0300

    Heroku

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -3,7 +3,6 @@ package com.devsuperior.dsmeta.controllers;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
 import com.devsuperior.dsmeta.services.SmsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
